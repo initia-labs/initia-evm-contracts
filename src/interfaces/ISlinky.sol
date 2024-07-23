247113c4524067e6dfab56658fa640bd6dabd5ff
Sawit Trisirisatayawong <sawit.tr@gmail.com>
2024-07-23 19:44:55 +0700

    feat: add slinky interface

---
+++ src/interfaces/ISlinky.sol
@@ -0,0 +1,16 @@
+pragma solidity ^0.8.24;
+
+interface ISlinky {
+    struct Price {
+        uint256 price;
+        uint256 timestamp;
+        uint64 height;
+        uint64 nonce;
+        uint64 decimal;
+        uint64 id;
+    }
+
+    function get_all_currency_pairs() external returns (string memory);
+    function get_price(string memory base, string memory quote) external returns (Price memory);
+    function get_prices(string[] memory pair_ids) external returns (Price[] memory);
+}
